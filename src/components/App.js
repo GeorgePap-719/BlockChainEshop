5f0be492e82de21d50899310fdb1513482f72790
GeorgePap-719 <g_kr0s@yahoo.gr>
2020-01-19 22:07:17 +0200

    doing pt5, react js stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,69 @@
-
 import React, { Component } from 'react';
-import Web3 from 'web3'
-import logo from '../logo.png';
-import './App.css';
-import Marketplace from '../abis/Marketplace.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import Web3 from 'web3';
+//import logo from '../logo.png';
+//import './App.css';
+
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+  async componentWillMount(){
+    await this.loadweb3
+    await this.loadBlockchainData
   }
 
-  async loadWeb3() {
+  async loadweb3(){
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
     }
     else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
+        window.web3 = new Web3(window.web3.currentProvider);
     }
+    // Non-dapp browsers...
     else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
   }
 
   async loadBlockchainData() {
     const web3 = window.web3
-    // Load account
+    //load accounts
     const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Marketplace.networks[networkId]
-    if(networkData) {
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      this.setState({ marketplace })
-      const productCount = await marketplace.methods.productCount().call()
-      this.setState({ productCount })
-      // Load products
-      for (var i = 1; i <= productCount; i++) {
-        const product = await marketplace.methods.products(i).call()
-        this.setState({
-          products: [...this.state.products, product]
-        })
-      }
-      this.setState({ loading: false})
-    } else {
-      window.alert('Marketplace contract not deployed to detected network.')
-    }
-  }
+    console.log(accounts)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      productCount: 0,
-      products: [],
-      loading: true
-    }
 
-    this.createProduct = this.createProduct.bind(this)
-    this.purchaseProduct = this.purchaseProduct.bind(this)
-  }
-
-  createProduct(name, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-  }
-
-  purchaseProduct(id, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
   }
 
   render() {
+
     return (
       <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="https://github.com/GeorgePap-719/BlockChainEshop"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dapp eShop Blockchain Marketplace
+              </a>
+            </nav>
+          <div className="container-fluid mt-5">
+            <div className="row">
+        <div  className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex">
-              { this.state.loading
-                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main
-                  products={this.state.products}
-                  createProduct={this.createProduct}
-                  purchaseProduct={this.purchaseProduct} />
-              }
             </main>
+            <h1>Dapp eShop</h1>
+            <p>
+            Edit <code>src/components/App.js</code> and save to reload.
+            </p>
+            </div>
           </div>
         </div>
+
       </div>
+    </div>
     );
   }
 }
