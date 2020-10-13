f119b0c779cac4456a086ee9d8cf4bf1d16e511b
GeorgePap-719 <g_kr0s@yahoo.gr>
2020-10-13 13:40:58 +0300

    Edited .gitignore file

--- src/components/App.js
+++ src/components/App.js
@@ -1,111 +1,139 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Web3 from 'web3';
 //import logo from '../logo.png';
-//import './App.css';
+import './css/App.css';
 import eShop from '../abis/eShop.json';
+import blindAuction from '../abis/BlindAuction.json';
 import Navbar from './Navbar';
 import Main from './Main';
 
+process.title = eShop;
+process.title = blindAuction;
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadweb3()
-    await this.loadBlockchainData()
-  }
+    async componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockChainData()
+    }
 
-  async loadweb3() {
+    //Common pattern for connecting the app to web3.
+    async loadWeb3() {
 
-      if (window.ethereum) {
-          window.web3 = new Web3(window.ethereum)
-          await window.ethereum.enable()
-      }
-      else if (window.web3) {
-          window.web3 = new Web3(window.web3.currentProvider)
-      }
-      // Non-dapp browsers...
-      else {
-          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-      }
-  }
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        // Non-dapp browsers...
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    }
 
-  async loadBlockchainData() {
-    const web3 = window.web3
-    //load accounts
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData =  eShop.networks[networkId]
+    async loadBlockChainData() {
+        const web3 = window.web3
+        //load accounts
+        const accounts = await web3.eth.getAccounts()
+        this.setState({account: accounts[0]})
+        const networkId = await web3.eth.net.getId()
 
-    if(networkData) {
-      const eshop = web3.eth.Contract(eShop.abi, networkData.address)
-      this.setState({ eshop })
-      const productCount = await eshop.methods.productCount().call()
-      this.setState({ productCount })
-      //Load products checkValidServiceWorker
-      for (var i = 1; i <= productCount; i++) {
-        const product = await eshop.methods.products(i).call()
-        this.setState({
-          products: [...this.state.products, product]
+        const networkDataEshop = eShop.networks[networkId]
+        const networkDataBid = blindAuction.networks[networkId]
+        console.log(networkDataBid)
+
+        //TODO this networkdata bid is empty
+        if (networkDataEshop && networkDataBid) {
+            //Loading the contracts
+            const eshop = web3.eth.Contract(eShop.abi, networkDataEshop.address)
+            this.setState({eshop})
+            // eslint-disable-next-line
+            const blindAuction = web3.eth.Contract(blindAuction.abi, networkDataBid.address)
+            this.setState({blindAuction})
+
+            //Loading arrays from the blockchain
+            const productCount = await eshop.methods.productCount().call()
+            this.setState({productCount})
+            //Load products checkValidServiceWorker
+            for (var i = 1; i <= productCount; i++) {
+                const product = await eshop.methods.products(i).call()
+                this.setState({
+                    products: [...this.state.products, product]
+                })
+            }
+            console.log(productCount.toString())
+            this.setState({loading: false})
+        } else {
+            window.alert("eShop contract can not be deployed to detected network")
+        }
+
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '',
+            productCount: 0,
+            products: [],
+            loading: true
+        }
+        this.createProduct = this.createProduct.bind(this)
+        this.purchaseProduct = this.purchaseProduct.bind(this)
+        this.bidProduct = this.bidProduct.bind(this)
+    }
+
+    //Function for calling the corresponding function inside the smart contract
+    createProduct(name, price) {
+        this.setState({loading: true})
+        this.state.eshop.methods.createProduct(name, price).send({from: this.state.account})
+            .once('receipt', (receipt) => {
+
+            })
+        this.setState({loading: false})
+        //
+    }
+
+    //Function for calling the corresponding function inside the smart contract
+    purchaseProduct(id, price) {
+        this.setState({loading: true})
+        this.state.eshop.methods.purchaseProduct(id)
+            .send({from: this.state.account, value: price}).once
+        ('receipt', (receipt) => {
+
         })
-      }
-      console.log(productCount.toString())
-      this.setState({ loading: false })
-    } else {
-      window.alert("eShop contract not deployed to detected network")
+        this.setState({loading: false})
+        //  window.location.reload(false);
     }
 
-  }
+    //Function for calling the corresponding function inside the smart contract
+    bidProduct(id) {
+        // this.setState({loading: true})
+        //this.state.esho
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      productCount: 0,
-      products: [],
-      loading: true
     }
-    this.createProduct = this.createProduct.bind(this)
-    this.purchaseProduct = this.purchaseProduct.bind(this)
-  }
 
-  createProduct(name, price) {
-    this.setState({ loading: true })
-    this.state.eshop.methods.createProduct(name, price).send({from: this.state.account}).once('receipt',(receipt) =>  {
-    this.setState({ loading: false })
-    })
-  }
-
-  purchaseProduct(id, price) {
-    this.setState({ loading: true })
-    this.state.eshop.methods.purchaseProduct(id)
-    .send({from: this.state.account, value: price}).once
-    ('receipt',(receipt) =>  {
-
-    })
-    this.setState({ loading: false })
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-          <div className="container-fluid mt-5">
-            <div className="row">
-              <main role="main" className="col-lg-12 d-flex">
-              { this.state.loading
-                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                 : <Main
-                  products={this.state.products}
-                  createProduct={this.createProduct}
-                  purchaseProduct={this.purchaseProduct} />
-               }
-              </main>
+    render() {
+        return (
+            <div>
+                <Navbar account={this.state.account}/>
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <main role="main" className="col-lg-12 d-flex">
+                            {this.state.loading
+                                ?
+                                <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                                : <Main
+                                    products={this.state.products}
+                                    createProduct={this.createProduct}
+                                    purchaseProduct={this.purchaseProduct}/>
+                            }
+                        </main>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Main extends Component {
 
@@ -62,8 +62,8 @@ class Main extends Component {
                           this.props.purchaseProduct(event.target.name, event.target.value)
                         }}>
                         Buy
-                    </button>
-                    : null
+                      </button>
+                      : null
                   }
                   </td>
                 </tr>
@@ -72,9 +72,69 @@ class Main extends Component {
 
           </tbody>
         </table>
+          <h3>Bid On Products For Shipping</h3>
+          <table className="table">
+              <thead>
+              <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Owner</th>
+              </tr>
+              </thead>
+              <tbody id="ProductsForBiding">
+              {this.props.products.map((product, key) => {
+                  return (
+                      <tr key={key}>
+                          <th scope="row">{product.id.toString()}</th>
+                          <td>{product.name}</td>
+                          <td>{window.web3.utils.fromWei
+                          (product.price.toString(), 'Ether')} Eth
+                          </td>
+                          <td>{product.owner}</td>
+                          <td>
+                              {product.purchased
+                                  ? <button
+                                      name={product.id}
+                                      value={product.price}
+                                      onClick={(event) => {
+                                          //this.props.purchaseProduct(event.target.name, event.target.value)
+                                          //TODO impl blind auction house
+                                      }}>
+                                      Bid
+                                  </button>
+                                  : null
+                              }
+                          </td>
+                      </tr>
+                  )
+              })
+
+              }
+
+              </tbody>
+          </table>
+
       </div>
     );
   }
 }
 
 export default Main;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
